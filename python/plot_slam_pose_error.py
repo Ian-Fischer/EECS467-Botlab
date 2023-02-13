bd694ce7bdfcda0ae0d7bd0ab434ae3f215c2d7a
Ashwin Gupta <ashwingupta2000@gmail.com>
2023-02-13 10:04:20 -0500

    bump

--- python/plot_slam_pose_error.py
+++ python/plot_slam_pose_error.py
@@ -5,7 +5,7 @@ import lcm
 import matplotlib.pyplot as plt
 import numpy as np
 
-from lcmtypes import mbot_encoder_t, mbot_motor_command_t, timestamp_t, odometry_t
+from lcmtypes import pose_xyt_t, odometry_t
 
 sys.path.append("lcmtypes")
 
@@ -18,50 +18,43 @@ file = sys.argv[1]
 log = lcm.EventLog(file, "r")
 
 odom_data = []
-odom_init = 0
-
+odom_utime = []
+slam_data = []
+slam_utime = []
 
 for event in log:
+    
     if event.channel == "ODOMETRY":
         odom_msg = odometry_t.decode(event.data)
-        if odom_init == 0:
-            odom_start_time = odom_msg.utime
-            odom_init = 1
+        odom_data.append(odom_msg.x)
+        odom_data.append(odom_msg.y)
+        odom_data.append(odom_msg.theta)
+        odom_utime.append(odom_msg.utime)
+    if event.channel == "SLAM_POSE":
+        slam_msg = pose_xyt_t.decode(event.data)
+        slam_data.append(slam_msg.x)
+        slam_data.append(slam_msg.y)
+        slam_data.append(slam_msg.theta)
+        slam_utime.append(slam_msg.utime)
 
-    if event.channel == "MBOT_TIMESYNC":
-        timesync_msg = timestamp_t.decode(event.data)
-        timesync_data = np.append(timesync_data, np.array([[
-            (timesync_msg.utime)/1.0E6,
-        ]]), axis=0)
+i = np.argmin( np.abs(slam_utime[0] - np.array(odom_utime)))
+print(f"argmin {i}")
+q = np.min( np.abs(slam_utime[0] - np.array(odom_utime)))
+print(f"min {q}")
 
-odom_data = np.vstack(odom_data)
+slam_data = np.array(slam_data).reshape(-1, 3)
+odom_data = np.array(odom_data).reshape(-1, 3)[i:i+slam_data.shape[0], :]
 
-def plot_xy(odom_data):
-    xs = odom_data[:, 0]
-    ys = odom_data[:, 1]
-    fig = plt.plot(xs - 0.1, ys -0.4, "b-", label="odom")
-    #plt.plot([0, -1, -1, 0, 0], [0, 0, -1, -1, 0], "r", label="ground truth")
-    plt.legend()
-    plt.xlabel("X (meters)")
-    plt.ylabel("Y (meters)")
-    plt.show()
+error = np.sqrt((odom_data[:, 0] - slam_data[:, 0])**2 + (odom_data[:, 1] - slam_data[:, 1])**2)
 
-def plot_xt(odom_data):
-    xs = odom_data[:, 0]
-    ys = odom_data[:, 1]
-    plt.plot(np.arange(xs.size), xs, "b-")
-    plt.xlabel("time (seconds)")
-    plt.ylabel("X (meters)")
-    plt.show()
+#error = np.sqrt((odom_data[:, 1] - slam_data[:, 1])**2)
 
-def plot_theta_t(odom_data):
-    thetas = odom_data[:, 2]
-    plt.plot(np.arange(thetas.size), thetas, "r-")
-    plt.xlabel("time (seconds)")
-    plt.ylabel("Heading (radians)")
-    plt.show()
+fig = plt.plot(slam_utime, error, "b-", label="odom")
+plt.legend()
+plt.xlabel("utime")
+plt.ylabel("error in position (m)")
+plt.show()
 
-#plot_theta_t(odom_data)
-plot_xy(odom_data)
+
 
 
